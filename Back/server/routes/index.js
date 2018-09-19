1832502b7f31b5c418309625e0a30e9e3fa2bd57
Nikolai Smekalin <Zerfogame@yandex.ru>
2018-09-19 16:49:04 +0300

    Добавил прототипы двух новых таблиц

--- Back/server/routes/index.js
+++ Back/server/routes/index.js
@@ -15,5 +15,5 @@ module.exports = app => {
 			secret: 'SUPERSECTER'
 		})
 	);
-	logout(app)
+	logout(app);
 };
--- Back/server/routes/v1/auth/refresh.js
+++ Back/server/routes/v1/auth/refresh.js
@@ -6,7 +6,7 @@ const uuid = require('uuid/v4');
 module.exports = app => {
 	app.post('/api/v1/refresh', async (req, res) => {
     await User.sync();
-    const { refreshToken  } = req.body;
+    const { refreshToken } = req.body;
     const user = await User.findOne({ refreshToken });
     if (!user || !req.body.refreshToken) {
       res.status(404).send({
