912d853721923b6c4dd86397c4358386e863d283
Nikolay Smekalin <Zerfogame@yandex.ru>
2018-12-20 16:16:34 +0300

    fix

--- Back/server/helpers/searchComputer.js
+++ Back/server/helpers/searchComputer.js
@@ -2,9 +2,9 @@ const Computer = require('../../database/schemas/computer');
 
 const searchComputer = async data => {
   await Computer.sync();
-  const Computer = await Computer.findOne({ where: { ...data } });
-  if (!Computer) return 'Error';
-  return Computer;
+  const сomputer = await Computer.findOne({ where: { ...data } });
+  if (!сomputer) return 'Error';
+  return сomputer;
 }
 
 module.exports = searchComputer;
